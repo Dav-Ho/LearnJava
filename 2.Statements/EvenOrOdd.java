0833bf57feb95631ae8b828ec297ae5234377a8b
david <david@davids-MacBook-Pro-2.local>
2017-05-15 17:45:18 -0400

    Adding folders introductionand statements with problem/solution

---
+++ 2.Statements/EvenOrOdd.java
@@ -0,0 +1,17 @@
+// Create a program that will determine whether the number is even or odd.
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+
+		int number = 3;
+
+		if (number % 2 == 0) {
+			System.out.println(number + " is even!");
+		}
+		else {
+			System.out.println(number + " is odd!");
+		}
+
+		}
+}
---
+++ 2.Statements/ForLoopTest.java
@@ -0,0 +1,11 @@
+// Use a loop to increment a number from 1 to 20
+
+public class ForLoopTest {
+
+	public static void main(String[] args) {
+
+		for(int count = 1; count <= 20; count++) {
+			System.out.println("The Number is now " + count);
+		}
+	}
+}
